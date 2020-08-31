2b129747e0cca8f23a72ac63ac8c7d33807f22f5
La roch Jocelin <jocelinlaroch08@gmail.com>
2020-08-31 14:49:14 +0100

    First CardView

---
+++ src/Components/App/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Cards from '../Home/Card/index.jsx'
+
+
+class App extends Component {
+  render(){
+    return(
+      <Cards/>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './Components/App/App';
 import * as serviceWorker from './serviceWorker';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import  'bootstrap/dist/css/bootstrap.min.css' ;
+import App from './Components/App/App'
 
 ReactDOM.render(
   <React.StrictMode>
