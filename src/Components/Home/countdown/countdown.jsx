f7040a67f544cd4349dff3bb6b3b67ec4794fb16
La roch Jocelin <jocelinlaroch08@gmail.com>
2020-09-04 15:31:11 +0100

    count fix

--- src/Components/Home/countdown/countdown.jsx
+++ src/Components/Home/countdown/countdown.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState, Component} from "react";
-import { render } from "@testing-library/react";
+import React, { useEffect, useState,} from "react";
 
 
   function Countdown (){
