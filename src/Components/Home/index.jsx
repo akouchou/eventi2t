01271ad61c0b42bb58b83af6100d87b3d25e46b1
La roch Jocelin <jocelinlaroch08@gmail.com>
2020-09-10 07:27:02 +0100

    Premiere présentation

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -2,11 +2,13 @@ import React, {Fragment} from 'react';
 import Cards from './Card/index'
 import Carousel from './carousel/index'
 import Contact from './contact/index'
+import Videos from './video/index';
 
 const Home = () => {
     return(
        <Fragment>
            <Carousel />
+           <Videos/>
            <Cards />
            <Contact />
        </Fragment>
