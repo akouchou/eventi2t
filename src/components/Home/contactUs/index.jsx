ac0ade98c6e0cbec63ba1fe1738f5c6961c2bb56
La roch Jocelin <jocelinlaroch08@gmail.com>
2020-09-17 13:32:19 +0100

    Envoi du message contactUs via EmaiJS

--- src/components/Home/contactUs/index.jsx
+++ src/components/Home/contactUs/index.jsx
@@ -1,52 +1,102 @@
-import React, {Component} from 'react';
-import {Form, FormGroup, Input, Label, Button} from 'reactstrap';
+import React, { Component, Fragment } from 'react'
+import * as emailjs from 'emailjs-com'
+//import Layout from '../components/layout'
+import { Button, FormFeedback, Form, FormGroup, Label, Input } from 'reactstrap'
+import{ init } from 'emailjs-com';
 
 
-class ContactUs extends Component{
+class ContactUs extends Component {
+  state = {
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  }
+handleSubmit(e) {
+    e.preventDefault()
+    const { name, email, subject, message } = this.state
+    let templateParams = {
+        subject: subject,
+      name: name,
+      email: email,
+      message: message,
+     }
 
-    constructor(){
-        super()
-
-        this.state = {
-            name:'',
-            email:'',
-            message:''
-        }
-
-        this.handleChange = this.handleChange.bind(this)
-    }
-
-    handleChange = e => {
-        this.setState({[e.target.name]: e.target.value})
-    }
-
-    handleSubmit(e) {
-        e.preventDefault()
-
-        const {name, email, message} = this.state
-    }
-
-    render(){
-        return (
-
-            <Form onSubmit={this.handleSubmit} style={{width:'600px'}}>
-                <FormGroup>
-                    <Label for="name">Name:</Label>
-                    <Input type = "text" name = "name" onChange={this.handleChange}/>
-                </FormGroup>
-                <FormGroup>
-                    <Label for="email">Email:</Label>
-                    <Input type = "email" name = "email" onChange={this.handleChange}/>
-                </FormGroup>
-                <FormGroup>
-                    <Label for="message">Message:</Label>
-                    <Input type = "textarea" name = "message" onChange={this.handleChange}/>
-                </FormGroup>
-                <Button>Envoyer</Button>
-            </Form>
-    
-        );
-    }
+     emailjs.send(
+        "service_u5w8aak",
+        "template_nt9qv2k",
+       templateParams,
+      'user_qsCaizYblaLOa904GGYVm'
+     )
+     this.resetForm()
+ }
+resetForm() {
+    this.setState({
+      name: '',
+      email: '',
+      subject: '',
+      message: '',
+    })
+  }
+handleChange = (param, e) => {
+    this.setState({ [param]: e.target.value })
+  }
+render() {
+    return (
+      <>
+        <Fragment>
+          <h1 className="p-heading1">Get in Touch</h1>
+          <Form onSubmit={this.handleSubmit.bind(this)}>
+            <FormGroup controlId="formBasicEmail">
+              <Label className="text-muted">Email address</Label>
+              <Input
+                type="email"
+                name="email"
+                value={this.state.email}
+                className="text-primary"
+                onChange={this.handleChange.bind(this, 'email')}
+                placeholder="Enter email"
+              />
+            </FormGroup>
+<FormGroup controlId="formBasicName">
+              <Label className="text-muted">Name</Label>
+              <Input
+                type="text"
+                name="name"
+                value={this.state.name}
+                className="text-primary"
+                onChange={this.handleChange.bind(this, 'name')}
+                placeholder="Name"
+              />
+            </FormGroup>
+<FormGroup controlId="formBasicSubject">
+              <Label className="text-muted">Subject</Label>
+              <Input
+                type="text"
+                name="subject"
+                className="text-primary"
+                value={this.state.subject}
+                onChange={this.handleChange.bind(this, 'subject')}
+                placeholder="Subject"
+              />
+            </FormGroup>
+<FormGroup controlId="formBasicMessage">
+              <Label className="text-muted">Message</Label>
+              <Input
+                type="textarea"
+                name="message"
+                className="text-primary"
+                value={this.state.message}
+                onChange={this.handleChange.bind(this, 'message')}
+              />
+            </FormGroup>
+<Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Fragment>
+      </>
+    )
+  }
 }
-
 export default ContactUs
