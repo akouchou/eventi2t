bffbaa7d2ef5a932f7139b76bd87c305ab3ec5ea
La roch Jocelin <jocelinlaroch08@gmail.com>
2020-09-21 14:45:35 +0100

    commit pour tout prendre

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 import React from 'react';
-
 import Header from '../Home/header';
 import Home from '../Home/index'
 import Event from '../Event/index'
@@ -26,32 +24,3 @@ function App() {
   );
 }
 export default App;
-=======
-import React from 'react';
-
-import Header from '../Home/header';
-import Home from '../Home/index'
-import Event from '../Event/index'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom' 
-import Footer from '../Home/footer';
-import MapCard from '../Event/CardMaps';
-
-function App() {
-  return (
-    <Router>
-
-      <Header/>
-      
-      <Switch>
-      <Route path="/event/:id" component={Event} />
-      <Route exact path="/" component={Home} />
-      <Route path="/map/:id" component={MapCard} />
-        
-      </Switch>
-      
-      <Footer/>
-    </Router>
-  );
-}
-export default App;
->>>>>>> 10eb53c94dfc8ed2bcc76681fd8f9f64161c7e6a
--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -1,19 +1,6 @@
-<<<<<<< HEAD
 import Firebase from './firebase'
-
 import FirebaseContext from './context'
 
 export default Firebase 
 
 export { FirebaseContext }
-
-=======
-import Firebase from './firebase'
-
-import FirebaseContext from './context'
-
-export default Firebase 
-
-export { FirebaseContext }
-
->>>>>>> 10eb53c94dfc8ed2bcc76681fd8f9f64161c7e6a
